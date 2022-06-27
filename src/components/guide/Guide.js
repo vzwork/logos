d1cc3ece061f02f3f1d6d86cbe46f83d3bb9830b
vzschool <vladislav.zakharov.school@gmail.com>
2022-06-27 06:31:55 -0700

    reset

---
+++ src/components/guide/Guide.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../navbar/Navbar';
+
+export default function Guide () {
+  return (
+    <div className='Guide'>
+      <Navbar />
+      Guide goes here!
+    </div>
+  );
+}
